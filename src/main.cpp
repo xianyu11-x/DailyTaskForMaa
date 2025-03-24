70eb8a34ee88bc849b262ecddb6033ec813abd82
xianyu11-x <446162056@qq.com>
2025-03-24 19:58:18 +0800

    feat:添加SSReOpen任务

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,37 @@
 #include "co_async/awaiter/task.hpp"
+#include "conf/conf.h"
 #include "levelManager/levelManager.h"
 #include "mysqlConnectPool/sqlConnectPool.h"
 #include "router/router.h"
 #include <co_async/co_async.hpp>
 #include <co_async/std.hpp>
-#include "conf/conf.h"
 
 using namespace co_async;
 using namespace std::literals;
 
-static Task<Expected<>> amain(std::string serveAt,std::string confPath) {
+static Task<Expected<>> amain(std::string serveAt, std::string confPath) {
   co_await co_await stdio().putline("listening at: "s + serveAt);
   auto listener = co_await co_await listener_bind(
       co_await AddressResolver().host(serveAt).resolve_one());
 
-  auto confPathStr = make_path(".",confPath);
+  auto confPathStr = make_path(".", confPath);
   auto confStr = co_await co_await file_read(confPathStr);
   auto confManager = confManager::GetInstance();
   confManager->init(confStr);
 
-  auto permanentLevelpath = make_path(".",confManager->getPermantLevelPath());
+  auto permanentLevelpath = make_path(".", confManager->getPermantLevelPath());
   auto permanentLevelStr = co_await co_await file_read(permanentLevelpath);
   auto levelManager = levelManager::GetInstance();
   levelManager->setPermanentLevel(permanentLevelStr);
 
-  //TODO:使用配置文件统一处理配置信息
-  co_await co_await stdio().putline("connecting db"s );
+  // TODO:使用配置文件统一处理配置信息
+  co_await co_await stdio().putline("connecting db"s);
   auto dbInfo = confManager->getDbInfo();
   auto conn = connectionPool::GetInstance();
-  conn->init(dbInfo.url, dbInfo.user, dbInfo.password, dbInfo.databaseName, dbInfo.port, dbInfo.maxConn);
+  conn->init(dbInfo.url, dbInfo.user, dbInfo.password, dbInfo.databaseName,
+             dbInfo.port, dbInfo.maxConn);
   HTTPServer server;
-  co_await co_await stdio().putline("setting route"s );
+  co_await co_await stdio().putline("setting route"s);
   setAllRoute(server);
 
   struct Worker {
@@ -70,7 +71,7 @@ static Task<Expected<>> amain(std::string serveAt,std::string confPath) {
   for (std::size_t i = 0; i < workers.size(); ++i) {
     workers[i].start(i);
   }
-  co_await co_await stdio().putline("start loop"s );
+  co_await co_await stdio().putline("start loop"s);
   std::size_t i = 0;
   while (true) {
     if (auto income = co_await listener_accept(listener)) [[likely]] {
@@ -90,7 +91,7 @@ int main(int argc, char **argv) {
   if (argc > 1) {
     serveAt = argv[1];
     confPath = argv[2];
-  } 
-  co_main(amain(serveAt,confPath));
+  }
+  co_main(amain(serveAt, confPath));
   return 0;
 }
